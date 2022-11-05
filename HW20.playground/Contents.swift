628a83433fa6aac49549a5a59779f1710db06b79
Anton Khrapov <froe054@mail.ru>
2022-11-05 20:07:33 +0700

    End of Work

--- HW20.playground/Contents.swift
+++ HW20.playground/Contents.swift
@@ -1,3 +1,102 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// MARK: - Struct with URL
+
+struct CardURL {
+    enum CardName: String {
+        case opt
+        case blackLotus = "black_lotus"
+    }
+    
+    private var components = URLComponents()
+    
+    private let scheme = "https"
+    private let host = "api.magicthegathering.io"
+    private let path = "/v1/cards"
+    private let queryName = "name"
+    private let queryValue: CardName
+    
+    init(with name: CardName) {
+        self.queryValue = name
+        set()
+    }
+    
+    private mutating func set() {
+        components.scheme = scheme
+        components.host = host
+        components.path = path
+        components.queryItems = [URLQueryItem(name: queryName, value: queryValue.rawValue)]
+    }
+    
+    public func getURL() -> URL? {
+        components.url
+    }
+}
+
+// MARK: - Decodable Model
+
+struct Cards: Decodable {
+    let cards: [Card]
+}
+
+struct Card: Decodable {
+    let name: String
+    let manaCost: String?
+    let rarity: String
+    let setName: String
+    let type: String
+    let cmc: Double
+}
+
+// MARK: - Get data method
+
+func getData(urlRequest: URL?) {
+    guard let url = urlRequest else { return }
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        guard let response = response as? HTTPURLResponse else {
+            print("Проблемы с сетью")
+            return
+        }
+        switch response.statusCode {
+        case 200:
+            guard let data = data else { return }
+            do {
+                let decoded = try JSONDecoder().decode(Cards.self, from: data)
+                print(card: decoded.cards[0])
+            } catch {
+                print("Ошибка при декодировке JSON")
+            }
+        default:
+            if let error = error {
+                print("Ошибка \(error.localizedDescription)")
+            } else {
+                print("Статус код запроса - \(response.statusCode)")
+            }
+        }
+    }.resume()
+}
+
+// MARK: - Printer
+
+func print(card: Card?) {
+    if let card = card, var manacost = card.manaCost {
+        manacost = manacost.filter { $0 != "{" && $0 != "}" }
+        print("""
+        Название карты - \(card.name)
+        Стоимость - \(manacost)
+        Тип - \(card.type)
+        Редкость - \(card.rarity)
+        Название набора - \(card.setName)\n
+        """
+        )
+    }
+}
+
+// MARK: - Instances and Data responces
+
+let blackLotusURL = CardURL(with: .blackLotus).getURL()
+let optURL = CardURL(with: .opt).getURL()
+
+getData(urlRequest: blackLotusURL)
+getData(urlRequest: optURL)
+
